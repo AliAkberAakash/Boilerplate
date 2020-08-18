a1fdcc828fd9d5739a084973eea8c3fdc3125791
aliakberaakash <ali852609@gmail.com>
2020-08-18 13:28:19 +0600

    Added BaseViewModel

---
+++ app/src/main/java/com/aliakberaakash/boilerplate/core/ui/BaseViewModel.kt
@@ -0,0 +1,41 @@
+package com.aliakberaakash.boilerplate.core.ui
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import timber.log.Timber
+
+
+class BaseViewModel : ViewModel(), IViewModel {
+
+    val loader = MutableLiveData<Boolean>()
+
+    override fun onAny(owner: LifecycleOwner?, event: Lifecycle.Event?) {
+        Timber.d("${event?.name}")
+    }
+
+    override fun onCreate() {
+
+    }
+
+    override fun onDestroy() {
+
+    }
+
+    override fun onStart() {
+
+    }
+
+    override fun onStop() {
+
+    }
+
+    override fun onResume() {
+
+    }
+
+    override fun onPause() {
+
+    }
+}
